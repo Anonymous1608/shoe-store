cc8c91b89f3a25b5d4ef84c0a04c60f2632128c9
Aniket <aniketkalbande16@gmail.com>
2023-04-28 02:26:25 +0530

    Added category and product slug

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import HeroBanner from "@/components/HeroBanner"
+import ProductCard from "@/components/ProductCard"
 import Wrapper from "@/components/Wrapper"
+
 
 export default function Home() {
   return (
@@ -11,7 +13,31 @@ export default function Home() {
             <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
               Cushioning for your Miles
             </div>
+            <div className="text-md md:text-xl">
+            A lightweight Nike ZoomX midsole is combined with
+            increased stack heights to help provide cushioning
+            during extended stretches of running.
+            </div>
          </div>
+          {/* heading and paragaph end */}
+
+          {/* products grid start  */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0 ">
+              <ProductCard />
+              <ProductCard />
+              <ProductCard />
+              <ProductCard />
+              <ProductCard />
+              <ProductCard />
+              <ProductCard />
+              <ProductCard />
+              <ProductCard />
+              
+
+          </div>
+
+          {/* products grid end  */}
+
       </Wrapper>
     </main>
   )
